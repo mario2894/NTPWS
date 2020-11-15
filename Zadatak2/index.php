9ee16e2375c14c41d05bfe6f9e724c174fe8cbee
Mario Tušek <mario.tuse@gmail.com>
2020-11-15 20:50:44 +0100

    2. zadatak

---
+++ Zadatak2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" href="styles.css">
+
+		<title>Napredne tehnike programiranja web servisa</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name="description" content="Projektni zadatak">
+		<meta name="keywords" content="programiranje, web servisi">
+		<meta name="author" content="Mario Tušek">
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+	</head>
+<body>
+	<header>
+		<div class="hero-image"></div>
+		<nav>
+			<ul>
+			  <li><a href="index.php">Početna stranica</a></li>
+			  <li><a href="#">Novosti</a></li>
+			  <li><a href="#">Kontakt</a></li>
+			  <li><a href="#">O nama</a></li>
+			  <li><a href="#">Galerija</a></li>
+			</ul>
+		</nav>
+	</header>
+
+</body>
+</html>
