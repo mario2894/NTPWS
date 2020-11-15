a5eeec9c8e9e29d686790fefdfa6252ab63f1f13
Mario Tušek <mario.tuse@gmail.com>
2020-11-15 20:36:43 +0100

    1. zadatak

---
+++ Zadatak1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Napredne tehnike programiranja web servisa</title>
+		<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<meta name="description" content="Projektni zadatak">
+		<meta name="keywords" content="">
+		<meta name="author" content="Mario Tušek">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+	</head>
+<body>
+
+</body>
+</html>
