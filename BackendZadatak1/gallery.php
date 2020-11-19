359b788a5fa65b539bd08c1a80c903a609844353
Mario Tušek <mario.tuse@gmail.com>
2020-11-19 08:55:21 +0100

    1. zadatak - backend

---
+++ BackendZadatak1/gallery.php
@@ -0,0 +1,46 @@
+<?php
+	print '
+        <h1>Galerija</h1>
+		<div id="gallery">
+			<figure id="1">
+				<img src="gallery/gopro5.jpg" alt="GoPro HERO 5" title="GoPro HERO 5">
+				<figcaption>Akcijska kamera GoPro HERO 5<figcaption>
+			</figure>
+			<figure id="2">
+				<img src="gallery/hero_5_session.jpg" alt="GoPro HERO 5 Session" title="GoPro HERO 5 Session">
+				<figcaption>Akcijska kamera GoPro HERO 5 Session<figcaption>
+			</figure>
+			<figure id="3">
+				<img src="gallery/gopro6.jpg" alt="GoPro HERO 6" title="GoPro HERO 6">
+				<figcaption>Akcijska kamera GoPro HERO 6<figcaption>
+			</figure>
+			<figure id="4">
+				<img src="gallery/gopro7.jpg" alt="GoPro HERO 7 BLACK" title="GoPro HERO 7 BLACK">
+				<figcaption>Akcijska kamera GoPro HERO 7 BLACK<figcaption>
+			</figure>
+			<figure id="5">
+				<img src="gallery/gopro8.png" alt="GoPro HERO 8 BLACK" title="GoPro HERO 8 BLACK">
+				<figcaption>Akcijska kamera GoPro HERO 8 BLACK<figcaption>
+            </figure>
+            <figure id="6">
+				<img src="gallery/gopro9.jpg" alt="GoPro HERO 9 BLACK" title="GoPro HERO 9 BLACK">
+				<figcaption>Akcijska kamera GoPro HERO 9 BLACK<figcaption>
+            </figure>
+            <figure id="7">
+				<img src="gallery/dji_mavic_air.jpg" alt="DJI Mavic Air" title="DJI Mavic Air">
+				<figcaption>Dron DJI Mavic Air<figcaption>
+            </figure>
+            <figure id="8">
+				<img src="gallery/dji_mavic2.png" alt="DJI Mavic 2" title="DJI Mavic 2">
+				<figcaption>Dron DJI Mavic 2<figcaption>
+            </figure>
+            <figure id="9">
+				<img src="gallery/dji_phantom4.jpg" alt="DJI Phantom 4" title="DJI Phantom 4">
+				<figcaption>Dron DJI Phantom 4<figcaption>
+            </figure>
+            <figure id="10">
+				<img src="gallery/dji_matrice300rtk.jpg" alt="DJI Matrice 300 RTK" title="DJI Matrice 300 RTK">
+				<figcaption>Dron DJI Matrice 300 RTK<figcaption>
+			</figure>
+		</div>';
+		?>
